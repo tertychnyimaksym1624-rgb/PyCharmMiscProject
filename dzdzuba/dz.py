7e15396e833e8bd892426ed4a8b42976a6d74b33
TertMaxe <terticnijmaksim47@gmail.com>
2025-12-16 20:22:30 +0200

    asfasf

---
+++ dzdzuba/dz.py
@@ -0,0 +1,84 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import Counter
+
+# --- ВХІДНІ ---
+x = [4,7,6,8,5,4,5,8,7,6,9,4,3,8,6,5,5,8,10,5]
+n = len(x)
+
+# --- ВАРІАЦІЙНИЙ РЯД ---
+x_sort = np.sort(x)
+print(f"n = {n}")
+print(f"Варіаційний ряд: {x_sort}")
+
+# --- ЧАСТОТИ ---
+freq = Counter(x_sort)
+vals = sorted(freq.keys())
+cnts = [freq[v] for v in vals]
+
+print("\n--- Частоти ---")
+print(f"{'xi':<5} | {'ni':<5}")
+print("-" * 15)
+for v, c in zip(vals, cnts):
+    print(f"{v:<5} | {c:<5}")
+
+# --- ХАРАКТЕРИСТИКИ ---
+x_mean = np.mean(x)                    # 1
+var_n = np.var(x, ddof=0)              # 2
+var_n1 = np.var(x, ddof=1)             # 3
+std_n = np.std(x, ddof=0)              # 4
+std_n1 = np.std(x, ddof=1)             # 5
+x_range = np.max(x) - np.min(x)        # 6
+x_med = np.median(x)                   # 7
+x_mode = max(freq, key=freq.get)       # 8
+Q1 = x_sort[4]
+Q3 = x_sort[14]
+Q = (Q3 - Q1) / 2                      # 9
+cv = (std_n1 / x_mean) * 100           # 10
+
+# 11-12
+d = x - x_mean
+A = np.mean(d**3) / (std_n**3)
+E = (np.mean(d**4) / (std_n**4)) - 3
+print("-"*50)
+
+print("\n--- Числові характеристики ---")
+print(f"\n1)Вибірркове середнє:  x̄ = {x_mean}")
+print(f"2)Вибіркова дисперсія:  D = {var_n:.4f}")
+print(f"3)Підправлена дисперсія:  s² = {var_n1:.4f}")
+print(f"4)Вибіркове серднє квадратичне відхилення:  σ = {std_n:.4f}")
+print(f"5)Підправлене середнє квадратичне відхилення:  s = {std_n1:.4f}")
+print(f"6)Розмах вибірки:  R = {x_range}")
+print(f"7)Медіана:  Me = {x_med}")
+print(f"8)Мода:  Mo = {x_mode} ({freq[x_mode]} разів)")
+print(f"9)Квадратильне відхилення:  Q = {Q}")
+print(f"10)Коефіціієнт варіації: V = {cv:.2f}%")
+print(f"11)Коефіцієнт асиметрії: A = {A:.4f}")
+print(f"12)Ексцес для вибірки: E = {E:.4f}")
+print("-"*50)
+# --- ГРАФІКИ ---
+plt.figure(figsize=(16, 8))
+
+# Гістограма
+plt.subplot(1, 2, 1)
+plt.hist(x, bins=int(((min(x)+max(x))/2)+1),
+         edgecolor='black', align='left', rwidth=0.8)
+plt.title('Статистичний розподіл вибірки')
+plt.xlabel('Значення x')
+plt.ylabel('Частота n')
+plt.grid(axis='y', alpha=0.5)
+plt.xticks(vals)
+
+# Емпірична функція
+plt.subplot(1, 2, 2)
+x_cdf = np.sort(x)
+y_cdf = np.arange(1, n+1) / n
+plt.step(x_cdf, y_cdf, where='post')
+plt.title('Емпірична функція розподілу')
+plt.xlabel('Значення x')
+plt.ylabel('Fn(x)')
+plt.grid(True)
+plt.yticks(np.arange(0, 1.1, 0.1))
+
+plt.tight_layout()
+plt.show()
